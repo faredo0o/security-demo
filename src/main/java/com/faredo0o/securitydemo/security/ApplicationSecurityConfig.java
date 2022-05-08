6f546646453a8c391af67fb28c4dd07a77e573f9
Essam <mastermail@gmail.com>
2022-05-08 23:24:50 +0200

    After Adding JWT Auth

--- src/main/java/com/faredo0o/securitydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/faredo0o/securitydemo/security/ApplicationSecurityConfig.java
@@ -1,23 +1,25 @@
 package com.faredo0o.securitydemo.security;
 
+import com.faredo0o.securitydemo.jwt.JwtConfig;
+import com.faredo0o.securitydemo.jwt.JwtTokenVerifier;
+import com.faredo0o.securitydemo.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import javax.crypto.SecretKey;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.faredo0o.securitydemo.security.UserPermissions.STUDENT_WRITE;
 import static com.faredo0o.securitydemo.security.UserRoles.*;
 
 @Configuration
@@ -25,26 +27,30 @@ import static com.faredo0o.securitydemo.security.UserRoles.*;
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
    private PasswordEncoder passwordEncoder;
+   private final JwtConfig jwtConfig;
+   private final SecretKey secretKey;
 @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, JwtConfig jwtConfig, SecretKey secretKey) {
         this.passwordEncoder = passwordEncoder;
-    }
+    this.jwtConfig = jwtConfig;
+    this.secretKey = secretKey;
+}
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(),jwtConfig, secretKey))
+                .addFilterAfter(new JwtTokenVerifier(jwtConfig,secretKey),JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
                 .antMatchers("/api/*").hasRole(STUDENT.name())
-                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
-                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(STUDENT_WRITE.getPermission())
-                .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
                 .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+                .authenticated();
+
+
     }
 
 
